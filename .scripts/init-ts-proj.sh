48dd848fec8f7f696a2fef0886d81e0e3a098773
airglow923 <p.hyundeok76@gmail.com>
2021-02-13 11:08:08 +0800

    update script

--- .scripts/init-ts-proj.sh
+++ .scripts/init-ts-proj.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # For optimum usage, you can git clone your remote repository before executing
 # this script so that it is applied to package.json.
@@ -20,51 +20,29 @@ jq_inplace() {
   rm $TEMP
 }
 
-# nodejs and npm for launching TS code and package management
+# node is a runtime for JavaScript code
 # npm is dependent on nodejs so installing npm only is sufficient
 # jq is for JSON manipulation
-DEPS=('nodejs' 'npm' 'jq')
-MISSING_DEPS=()
+DEPS="node npm jq"
+MISSING_DEPS=""
 
-for DEP in "${DEPS[@]}"; do
-  if [ ! -x "$(which "$DEP")" ]; then
-    MISSING_DEPS+="$DEP"
+for DEP in ${DEPS}; do
+  if [ ! -x "$(which ${DEP})" ]; then
+    MISSING_DEPS="${MISSING_DEPS} ${DEP}"
   fi
 done
 
-# conduct update && install only if there are missing dependencies
 if [ ${#MISSING_DEPS[@]} -ne 0 ]; then
-  # determine package manager and install required packages
-
-  # Debian
-  if which apt-get; then
-    sudo apt-get update
-    sudo apt-get upgrade -y
-    sudo apt-get install $MISSING_DEPS
-  # Red Hat
-  elif which yum; then
-    sudo yum update
-    sudo yum upgrade
-    sudo yum install $MISSING_DEPS
-  # Arch Linux
-  elif which pacman; then
-    sudo -Syu $MISSING_DEPS
-  # OpenSUSE
-  elif which zypper; then
-    sudo zypper refresh
-    sudo zypper update
-    sudo zypper install $MISSING_DEPS
-  else
-    echo "Cannot determine what package manager the system uses. Please edit the script for your needs."
-    exit 1;
-  fi
+  echo "Missing dependencies: ${MISSING_DEPS}"
+  exit 1;
 fi
 
 # initialize npm package
 npm init -y
 
 # install eslint and typescript for development
-npm i --save-dev eslint typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin nodemon prettier
+npm i --save-dev eslint typescript @typescript-eslint/parser \
+  @typescript-eslint/eslint-plugin nodemon prettier
 
 # get default tsconfig.json
 npx tsconfig.json
@@ -140,7 +118,7 @@ TEMP="module.exports = {
 echo "${TEMP}" > .eslintrc.js
 
 # add node_modules and dist directories to .gitignore
-if [[ -d .git ]]; then
+if [ -d .git ]; then
   TEMP='node_modules/
 dist/'
   echo "${TEMP}" > .gitignore

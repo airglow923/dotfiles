7f5c6c29b3dba08d19758a869dd00dfa01f00a9c
airglow923 <p.hyundeok76@gmail.com>
2025-07-16 19:26:53 +0900

    fix(nvim): do not use sqls for formatting

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -5,9 +5,16 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 local servers = require("configs.packages").lspconfig
 
+local on_attach_wo_format = function(client, bufnr)
+  client.server_capabilities.documentFormattingProvider = false
+  client.server_capabilities.documentRangeFormattingProvider = false
+
+  on_attach(client, bufnr)
+end
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = on_attach,
+    on_attach = lsp == "sqls" and on_attach_wo_format or on_attach,
     on_init = on_init,
     capabilities = capabilities,
   }

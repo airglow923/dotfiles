c9d72934afee8d84d3aa2bf178c2138cb32662e3
airglow923 <p.hyundeok76@gmail.com>
2026-03-13 02:52:18 +0900

    config(nvim): let pyright decide which python to use among different version management systems

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -12,11 +12,35 @@ local on_attach_wo_format = function(client, bufnr)
   on_attach(client, bufnr)
 end
 
+local get_python_path = function()
+  -- pyenv
+  local pyenv_path = vim.fn.system("pyenv which python"):gsub("\n", "")
+  if vim.fn.filereadable(pyenv_path) == 1 then
+    return pyenv_path
+  end
+
+  -- virtualenv
+  if vim.env.VIRTUAL_ENV then
+    return vim.env.VIRTUAL_ENV .. "/bin/python"
+  end
+
+  -- system
+  return vim.fn.exepath "python3" or vim.fn.exepath "python" or "python"
+end
+
 for _, server in ipairs(servers) do
+  local settings = {}
+
+  if server == "pyright" then
+    settings.python = { pythonPath = get_python_path() }
+    vim.print(get_python_path())
+  end
+
   vim.lsp.config(server, {
     on_attach = server == "sqls" and on_attach_wo_format or on_attach,
     on_init = on_init,
     capabilities = capabilities,
+    settings = settings,
   })
 
   vim.lsp.enable(server)
